538164755d7499e9eb50fc2dc3f3fa2264a389c5
Stephen Li <stephen.liziyun@gmail.com>
2014-05-01 21:35:53 -0400

    Assn 1.3

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -49,5 +49,15 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) {
+      1
+    } else if (money < 0) {
+      0
+    } else if (money >=1 && coins.isEmpty) {
+      0
+    } else {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins.tail)
+    }
+  }
 }
